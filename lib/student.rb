be2a210bfa9a330c512812219f85e90731179380
Robin Tram <robin.tram@gmail.com>
2018-10-19 18:38:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
 
   def self.new_from_db(row)
     student = Student.new 
-    binding.pry
+    # binding.pry
     student.id = row[0]
     student.name = row[1]
     student.grade = row[2]
