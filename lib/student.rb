0732e70910ea4f248f1fb2b6b10ebde0ac54de75
Robin Tram <robin.tram@gmail.com>
2018-10-19 19:02:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -83,4 +83,17 @@ class Student
       self.new_from_db(row)
     end 
   end 
+  
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+    SELECT * 
+    FROM students 
+    WHERE grade = 10
+    LIMIT ?
+    SQL
+    
+    DB[:conn].execute(sql, x).collect do |row|
+      self.new_from_db(row)
+    end 
+  end 
 end
