fdc0fb026b1f21ab4f271db32b33a2969373e9e9
Robin Tram <robin.tram@gmail.com>
2018-10-19 18:37:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,7 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # binding.pry
+    binding.pry
     student = Student.new 
     student.id = row[0]
     student.name = row[1]
