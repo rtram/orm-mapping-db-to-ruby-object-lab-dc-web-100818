144ae97fd6f5c62de66c8a339ff97d8e07e30e8e
Robin Tram <robin.tram@gmail.com>
2018-10-19 19:04:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -109,4 +109,16 @@ class Student
       self.new_from_db(row)
     end[0]
   end 
+  
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+    SELECT * 
+    FROM students 
+    WHERE grade = ?
+    SQL
+    
+    DB[:conn].execute(sql,grade).collect do |row|
+      self.new_from_db(row)
+    end 
+  end 
 end
